6ec13c8d6768f917e008596153e9893d50a1a2dd
Avinash Kumar <cursivedgreat@gmail.com>
2014-10-07 23:37:08 +0530

    Initial Commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by Avinash Kumar on 07/10/14.
+//  Copyright (c) 2014 Avinash Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by Avinash Kumar on 07/10/14.
+//  Copyright (c) 2014 Avinash Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
